ab202dfc3ba9d12e54b1ff7597b052519ea1a600
ifilipis <40601736+ifilipis@users.noreply.github.com>
2025-11-13 21:15:10 +0200

    Use timm cosine scheduler with restarts

--- toolkit/scheduler.py
+++ toolkit/scheduler.py
@@ -1,6 +1,41 @@
 import torch
 from typing import Optional
+from timm.scheduler.cosine_lr import CosineLRScheduler
 from diffusers.optimization import SchedulerType, TYPE_TO_SCHEDULER_FUNCTION, get_constant_schedule_with_warmup
+
+
+class _TimmCosineWithRestartsScheduler(CosineLRScheduler):
+    def __init__(self, optimizer: torch.optim.Optimizer, **kwargs):
+        if 'total_iters' in kwargs:
+            kwargs.setdefault('t_initial', kwargs.pop('total_iters'))
+        if 'max_iterations' in kwargs:
+            kwargs.setdefault('t_initial', kwargs.pop('max_iterations'))
+        if 'T_0' in kwargs:
+            kwargs.setdefault('t_initial', kwargs.pop('T_0'))
+        if 'T_mult' in kwargs:
+            kwargs.setdefault('cycle_mul', kwargs.pop('T_mult'))
+        if 'eta_min' in kwargs:
+            kwargs.setdefault('lr_min', kwargs.pop('eta_min'))
+
+        if 't_initial' not in kwargs:
+            raise ValueError("t_initial (or total_iters/max_iterations/T_0) must be provided for cosine_with_restarts scheduler")
+
+        super().__init__(optimizer, **kwargs)
+        self._num_updates = -1
+
+    def step(self, epoch: Optional[int] = None, metric: Optional[float] = None):
+        if epoch is None:
+            self._num_updates += 1
+        else:
+            self._num_updates = epoch
+        return super().step_update(self._num_updates, metric=metric)
+
+    def step_update(self, num_updates: Optional[int] = None, metric: Optional[float] = None):
+        if num_updates is None:
+            self._num_updates += 1
+        else:
+            self._num_updates = num_updates
+        return super().step_update(self._num_updates, metric=metric)
 
 
 def get_lr_scheduler(
@@ -15,11 +50,7 @@ def get_lr_scheduler(
             optimizer, **kwargs
         )
     elif name == "cosine_with_restarts":
-        if 'total_iters' in kwargs:
-            kwargs['T_0'] = kwargs.pop('total_iters')
-        return torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(
-            optimizer, **kwargs
-        )
+        return _TimmCosineWithRestartsScheduler(optimizer, **kwargs)
     elif name == "step":
 
         return torch.optim.lr_scheduler.StepLR(
